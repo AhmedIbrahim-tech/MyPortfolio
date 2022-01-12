51152333c70de14625a7e81deb9973b2122bc4b9
Ahmed Ibrahim <ebrahema89859@gmail.com>
2022-01-12 22:19:22 +0200

    Add project files.

---
+++ Infrastructure/DataContext.cs
@@ -0,0 +1,34 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Owner>().Property(X => X.Id).HasDefaultValueSql("NEWID()");
+            modelBuilder.Entity<PortfolioItem>().Property(X => X.Id).HasDefaultValueSql("NEWID()");
+
+            modelBuilder.Entity<Owner>().HasData(
+                new Owner()
+                {
+                    Id = Guid.NewGuid(),
+                    FullName = "Ahmed Ibrahim",
+                    Avatar = "avatar.jpg",
+                    Profil = ".NET Developer / Web Designer / Graphic Artist "
+                }
+                );
+        }
+        public DbSet<Owner> Owner { get; set; }
+        public DbSet<PortfolioItem> PortfolioItems { get; set; }
+    }
+}
