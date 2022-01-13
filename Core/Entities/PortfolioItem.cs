d41304a94f8636800473b3c2a8a908dddebb44db
Ahmed Ibrahim <ebrahema89859@gmail.com>
2022-01-13 17:13:13 +0200

    Update Picture Form Portfolio

--- Core/Entities/PortfolioItem.cs
+++ Core/Entities/PortfolioItem.cs
@@ -1,9 +1,13 @@
-﻿namespace Core.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Entities
 {
     public class PortfolioItem : EntityBase
     {
         public string ProjectName { get; set; }
         public string Description { get; set; }
+
+        [Display(Name = "Picture")]
         public string ImageUrl { get; set; }
     }
 }
